d8bf94326a512b0e62fac5f1c740027959272a96
2390858916@QQ.COM <2390858916@qq.com>
2023-05-13 22:05:15 +0800

    Changes element

--- vue-project/src/views/element/ElementView.vue
+++ vue-project/src/views/element/ElementView.vue
@@ -1,21 +1,111 @@
 <template>
 
     <div>
+        <!--        button 按钮-->
         <el-row>
-  <el-button>默认按钮</el-button>
-  <el-button type="primary">主要按钮</el-button>
-  <el-button type="success">成功按钮</el-button>
-  <el-button type="info">信息按钮</el-button>
-  <el-button type="warning">警告按钮</el-button>
-  <el-button type="danger">危险按钮</el-button>
-</el-row>
+            <el-button>默认按钮</el-button>
+            <el-button type="primary">主要按钮</el-button>
+            <el-button type="success">成功按钮</el-button>
+            <el-button type="info">信息按钮</el-button>
+            <el-button type="warning">警告按钮</el-button>
+            <el-button type="danger">危险按钮</el-button>
+        </el-row>
+
+        <!--        table 按钮-->
+
+        <el-table
+            :data="tableData"
+            style="width: 100%">
+            <el-table-column
+                prop="date"
+                label="日期"
+                width="180">
+            </el-table-column>
+            <el-table-column
+                prop="name"
+                label="姓名"
+                width="180">
+            </el-table-column>
+            <el-table-column
+                prop="address"
+                label="地址">
+            </el-table-column>
+        </el-table>
+
+<!--        分页代码-->
+        <el-pagination
+            background
+            layout="sizes, prev, pager, next,jumper , total"
+
+            @size-change="handleSizeChange"
+            @current-change="handleCurrentChange"
+            :total="1000">
+        </el-pagination>
+        <br><br>
+
+
+        <!-- Table -->
+        <el-button type="text" @click="dialogTableVisible = true">打开嵌套表格的 Dialog</el-button>
+
+        <el-dialog title="收货地址" :visible.sync="dialogTableVisible">
+            <el-table :data="gridData">
+                <el-table-column property="date" label="日期" width="150"></el-table-column>
+                <el-table-column property="name" label="姓名" width="200"></el-table-column>
+                <el-table-column property="address" label="地址"></el-table-column>
+            </el-table>
+        </el-dialog>
+
+        <el-button type="text" @click="outerVisible = true">点击打开外层 Dialog</el-button>
+
+        <el-dialog title="外层 Dialog" :visible.sync="outerVisible">
+            <el-dialog
+                width="30%"
+                title="内层 Dialog"
+                :visible.sync="innerVisible"
+                append-to-body>
+            </el-dialog>
+            <div slot="footer" class="dialog-footer">
+                <el-button @click="outerVisible = false">取 消</el-button>
+                <el-button type="primary" @click="innerVisible = true">打开内层 Dialog</el-button>
+            </div>
+        </el-dialog>
+
+<!--        表单组件-->
+
+
+        <br><br>
     </div>
 </template>
 
 <script>
-    export default{
-
+export default {
+    data(){
+        return{
+            tableData: [{
+                date: '2016-05-02',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1518 弄'
+            }],
+            gridData: [{
+                date: '2016-05-02',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1518 弄'
+            }],
+            dialogTableVisible: false,
+            dialogFormVisible: false,
+            outerVisible: false,
+            innerVisible: false
+        }
+    },
+    methods:{
+        handleSizeChange:function (val){
+            alert("每页变化记录" + val)
+        },
+        handleCurrentChange:function (val){
+            alert("页码变化记录" + val)
+        }
     }
+}
 </script>
 
 <style>
